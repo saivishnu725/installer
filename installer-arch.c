a4ca813a908db42649fb236cdb57abbe1d2ce148
Sai Vishnu <saivishnu725@gmail.com>
2021-07-21 11:07:55 +0530

    The list.

--- installer-arch.c
+++ installer-arch.c
@@ -3,6 +3,20 @@
 
 int main()
 {
-    printf("Hello, World!\n");
+    int brave;
+    int timeshift;
+    int vscode;
+    int gnome_tweaks;
+    int mvp;
+    int obs;
+    int kdenlive;
+    int android_studio;
+    int flutter;
+    int gimp;
+    int notion;
+    int qbittorrent;
+    int telegram;
+    int ulauncher;
+    int github_cli;
     return 0;
 }
